259ccf428873393832843aeb678a9180e4b2d613
Monirul Islam <mmislam027@gmail.com>
2025-12-18 14:18:48 +0600

    feat(chore): dockerize dashboard with nginx

--- apps/dashboard/src/content/dashboards/Users/UsersTable2.tsx
+++ apps/dashboard/src/content/dashboards/Users/UsersTable2.tsx
@@ -14,7 +14,6 @@ import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { visuallyHidden } from '@mui/utils';
 import * as React from 'react';
 import { useGetUsersQuery } from 'src/redux/features/user/userApiSlice';
 interface Data {
@@ -137,7 +136,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             >
               {headCell.label}
               {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
+                <Box component="span">
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </Box>
               ) : null}
